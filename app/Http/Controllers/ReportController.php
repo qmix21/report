7ef70f7160f97799221aacaeb2d3014e4dacf72e
qmix21 <james@ausgpl.com>
2018-07-23 16:07:43 +1000

    Testing output#2

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -10,7 +10,7 @@ class ReportController extends Controller
     public function mail()
     {
         $messages = LaravelGmail::message()->subject('Support Report - 22/07/18')->preload()->all();
-        return $messages->payload->parts[0]->body->data;
+        return $messages[0]->payload->parts[0]->body->data;
     }
     
 }
