317004b6add29ba017d6cb314ee05a6d062947bf
qmix21 <james@ausgpl.com>
2018-07-23 15:32:02 +1000

    added routes to go to mail. Updated ReportController for mail function

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use LaravelGmail;
 class ReportController extends Controller
 {
-    //
+    
+    public function mail()
+    {
+        $messages = LaravelGmail::message()->subject('test')->unread()->preload()->all();
+        return $messages;
+    }
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,14 @@ Route::get('/oauth/gmail', function (){
     return LaravelGmail::redirect();
 });
 
+Route::get('/mail',"ReportController@mail");
+
 Route::get('/oauth/gmail/callback', function (){
     LaravelGmail::makeToken();
-    return redirect()->to('/');
+    return redirect()->to('/mail');
 });
 
 Route::get('/oauth/gmail/logout', function (){
     LaravelGmail::logout(); //It returns exception if fails
-    return redirect()->to('/');
+    return redirect()->to('/mail');
 });
