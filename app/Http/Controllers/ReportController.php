bb7395ffa146430d9adc150d9672c2de77efeccf
qmix21 <james@ausgpl.com>
2018-07-23 16:09:47 +1000

    Seeing if Wildcards work?

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -9,7 +9,7 @@ class ReportController extends Controller
     
     public function mail()
     {
-        $messages = LaravelGmail::message()->subject('Support Report - 22/07/18')->preload()->all();
+        $messages = LaravelGmail::message()->subject('Support Report - *')->preload()->all();
         return $messages[0]->payload->parts[0]->body->data;
     }
     
