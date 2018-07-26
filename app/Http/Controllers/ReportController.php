5f1281a1726a69926c8b9931e542d0c3cef41ea7
James Mahon <james@ausgpl.com>
2018-07-26 14:55:43 +1000

    fix

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -24,8 +24,9 @@ class ReportController extends Controller
 
 		foreach($reports as $report)
 		{
-			$report->body = str_replace(' ', '-', base64_decode($report->body));
-			$report->body =  preg_replace('/[^A-Za-z0-9\-]/', '', base64_decode($report->body)); 
+			$report->body = base64_decode($report->body);
+			$report->body = str_replace(' ', '-', $report->body);
+			$report->body =  preg_replace('/[^A-Za-z0-9\-]/', '', $report->body); 
 		}
 		return $reports;
 	}
