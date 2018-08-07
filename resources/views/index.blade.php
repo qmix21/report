6e50c1178745aadc771fdfb9792f350f03534cb0
James Mahon <james@ausgpl.com>
2018-08-08 05:59:40 +1000

    fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,14 @@
 <html>
 <h1>Gmail API Reporting Page</h1>
-
-
 <div class="container">
+<div class="col-sm-8">
 <form action="/blog/public/test" method="GET">
 	<button type="submit">Get Emails</button>
+</form>
+</div>
+<div class="col-sm-8">
+<form action="/blog/public/userreport" method="GET">
+<button type="submit">Get Reports</button>
 </form>
 <hr>
 <form action="/blog/public/refresh" method="GET">
